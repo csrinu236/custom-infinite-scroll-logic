210e7d0ff79f1385710fab7fd991a460cd193f52
Chenna.Sreenu <csrinu236@gmail.com>
2024-10-21 14:00:29 +0530

    ok

--- pages/notes.js
+++ pages/notes.js
@@ -5,8 +5,8 @@ const Notes = () => {
     <div>
       <div className="comment">Excessive API query: every time network request goes when you come back to previously fetched route</div>
       <div className="comment">When All posts are fetched, fetching post/1 should not make another API call</div>
-      <div className="comment">Don't fetch again and again until it succeeds</div>
-      <div className="comment">Don't mutate data when db ka data changes</div>
+      <div className="comment">Don&apos;t fetch again and again until it succeeds</div>
+      <div className="comment">Don&apos;t mutate data when db ka data changes</div>
     </div>
   );
 };
